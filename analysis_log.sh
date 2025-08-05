1087bfe30c4dfdbcafb474c598c3980f10908e42
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2025-08-05 16:31:35 +0530

    new

--- analysis_log.sh
+++ analysis_log.sh
@@ -1,8 +1,11 @@
 find . -name "*.log" -mtime -1
+
+
 grep "ERROR" application.log
 grep -c "ERROR" application.log
 grep -c "FATAL" application.log
+
+
 grep -c "FATAL" system.log
 grep -c "CRITICAL" system.log
 grep "CRITICAL" system.log
-grep -c "CRITICAL" application.log
